c8950d990a032c83acee0e44d5d5b3fa018396c6
grandeurpawin <gpawin@byupathway.edu>
2026-09-10 17:34:17 +0800

    CSE 340 Service Network

--- server.js
+++ server.js
@@ -42,9 +42,11 @@ app.get("/", async (req, res) => {
 
 app.get("/organizations", async (req, res) => {
     const organizations = await getAllOrganizations();
+    console.log(organizations);
     const title = "Our Partner Organizations";
+    const description = "Discover the organizations we partner with to provide meaningful volunteer opportunities.";
 
-    res.render("organizations", { title, organizations});
+    res.render("organizations", { title, description, organizations });
 });
 
 app.get("/projects", async (req, res) => {
